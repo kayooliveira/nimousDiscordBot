6b3d77107a1c184f57e8f2addcc76845a1bfe3d3
Kayo Oliveira <35928107+kayooliveira1@users.noreply.github.com>
2020-05-29 00:33:46 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,53 +1,167 @@
 const Discord = require("discord.js");
-// Pegando a cfg do BOT
-const config = require("./config/config.json");
-// Loadando o fs
-const fs = require("fs")
-// Criando o Client do NimouzBOT
-const nimouz = new Discord.Client();
-// Criando o get dos comandos do BOT
-nimouz.commands = new Discord.Collection();
-// Criando um mapa pro MusicBot
-const active = new Map();
-// Localizando e carregando os comandos 
-fs.readdir("./comandos/", (err, files) => {
-	if(err) console.error(err);
+const db = require("quick.db");
+const config = require("./config.json");
+const fs = require("fs");
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
 
-	let arquivojs = files.filter(f => f.split(".").pop() === "js");
-	arquivojs.forEach((f, i) => {
-		let props = require(`./comandos/${f}`);
-		console.log(`Comando ${f} iniciado`);
-		nimouz.commands.set(props.help.name, props);
-	});
+// DATA PROS LOGS
+let date_ob = new Date();
+let date = ('0' + date_ob.getDate()).slice(-2);
+let month = ('0' + (date_ob.getMonth() + 1 )).slice(-2);
+let year = date_ob.getFullYear();
+let hours = date_ob.getHours();
+let minutes = date_ob.getMinutes();
+let seconds = date_ob.getSeconds();
+let str_data = `${date}/${month}/${year}`;
+let str_hora = `${hours}:${minutes}:${seconds}`;
+
+
+require('dotenv').config()
+
+fs.readdir("./comandos/", (err, files) => {
+  if (err) console.error(err);
+
+  let arquivojs = files.filter(f => f.split(".").pop() == "js");
+  arquivojs.forEach((f, i) => {
+    let props = require(`./comandos/${f}`);
+    console.log(`${f} ✓`);
+    client.commands.set(props.help.name, props);
+    props.help.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
 });
-// Setando a Rich Presence do BOT
-// creates an arraylist containing phrases you want your bot to switch through.
-const activities = [
-	"🥰 beijos pro6 🥰",
-	"💬 conversa fora 💬",
-	"discord.gg/D5nSxMA Vem pro meu planeta ❤️"
-]; 
-nimouz.on('ready', () => {
-	console.log(`BOT iniciado com sucesso em ${nimouz.guilds.cache.size} servidores!`)
-    setInterval(() => {
-        const index = Math.floor(Math.random() * (activities.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
-        nimouz.user.setActivity(activities[index], {type:'PLAYING'}); // sets bot's activities to one of the phrases in the arraylist.
-    }, 10000); // Runs this every 10 seconds.
+
+client.on("error", err => {
+  console.log(err.message);
 });
-// Comandos e respostas
-nimouz.on('message', message => {
-	if(message.author.bot) return;
-	if(message.channel.type === "dm") return message.reply("Olá, provavelmente você já está no meu servidor, porém eu ainda estou em desenvolvimento, portanto, mensagens diretas ainda não são suportadas, peço desculpas, mas estou tentando melhorar a todo momento ♥");
-	if(message.content.indexOf(config.prefix) !== 0) return;
-	// Setando o prefix do BOT
-	let prefix = config.prefix
-	let messageArray = message.content.split(" ");
-	let command = messageArray[0];
-	let args = messageArray.slice(1);
-	const sayMessage = args.join(" ");
-	let arquivocmd = nimouz.commands.get(command.slice(prefix.length)); 
-	if(arquivocmd) arquivocmd.run(nimouz, message, args, sayMessage, active);
+
+client.on("ready", () => {
+  console.log(`I'm ready!`);
+
+  const tabela = [
+    {name: "o canto dos pássaros", type: "LISTENING"},
+    {name: "Undertale", type: "PLAYING" },
+    {name: "animes :3", type: "WATCHING"},
+    {name: `meu papai se chama: kayoz1n#2361`, type: "LISTENING" },
+    {name: `meu prefixo é - » -ajuda`, type: "LISTENING" },
+    {name: "o meu papai programar", type: "WATCHING" },
+    {name: `amor para ${client.users.size} usuários`, type: "STREAMING", url: "https://twitch.tv/younguei"},
+    {name: `em ${client.guilds.size} servidores`, type: "PLAYING" },
+    {name: `Netflix com o boy`, type: "WATCHING" },
+    {name: `d!botinfo » Para mais informações sobre minha existência`,type: "LISTENING"},
+    {name: `boas energias para todos do servidor`,type: "STREAMING",url: "https://twitch.tv/younguei"},
+    {name: `-perfil » Crie um perfil dentro dos meus dados`, type: "LISTENING"},
+    {name: `meus códigos no Glitch.com`, type: "PLAYING"},
+    {name: `-about » Um texto explicando sobre mim`, type: "LISTENING"}
+  ];
+
+  function setStatus() {
+    let altstatus = tabela[Math.floor(Math.random() * tabela.length)];
+    client.user.setPresence({ game: altstatus });
+  }
+  setStatus();
+  setInterval(() => setStatus(), 10000);
+});
+
+
+client.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.cache.find(ch => ch.name === '𝑹𝒆𝒄𝒆𝒑𝒄̧𝒂̃𝒐');
+  if(!channel) return;
+  let embed = new Discord.MessageEmbed()
+  .setAuthor(client.user.username)
+  .setThumbnail(member.user.displayAvatarURL())
+  .setTitle(member.user.tag)
+  .setDescription('Chegou no servidor!')
+  .setColor('RANDOM')
+  .setFooter(`Entre no canal #𝑽𝒆𝒓𝒊𝒇𝒊𝒄𝒂𝒄̧𝒂̃𝒐 para verificar-se! | em: ${str_data} ás ${str_hora}`);
+  channel.send(embed);
+  
+let servidor = client.guilds.cache.get("708025424545972297")
+let unverified = servidor.roles.cache.get('715686151993950229')
+member.roles.add(unverified);
+});
+
+client.on('guildMemberRemove', member => {
+  const channel = member.guild.channels.cache.find(ch => ch.name === '𝑹𝒆𝒄𝒆𝒑𝒄̧𝒂̃𝒐');
+  if(!channel) return;
+  let embed = new Discord.MessageEmbed()
+  .setAuthor(client.user.username)
+  .setThumbnail(member.user.displayAvatarURL())
+  .setTitle(member.user.tag)
+  .setDescription('Saiu do servidor!')
+  .setColor('RANDOM')
+  .setFooter(`em: ${str_data} ás ${str_hora}`);
+  channel.send(embed);
+});
+
+client.on("guildDelete", guild => {
+  var canal = client.channels.get("698215400567079012");
+
+  canal.send(`<:blob_sad:687040398123728975> Me excluiram de um servidor! \`${guild.name} [${guild.id}]\`\n\n__**Informações**__\n👥 » Membros: \`${guild.memberCount}\`\n👑 » Proprietário: \`${guild.owner.user.tag}\`\n🏳️ » Região: \`${guild.region}\``);
+});
+
+client.on("message", async message => {
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+
+  if (message.mentions.has(client.user)) { 
+      message.channel.send(`Tá bem man?! **${message.author.username}**, está perdido? Tente utilizar: \`${config.prefix}ajuda\`!`);
+  }
+
+  var messagem = db.get(`messagem_${message.guild.id}`);
+  var idmessage = db.get(`messageid_${message.guild.id}`);
+
+  if (message.channel.id === idmessage) {
+    if (message.content.startsWith(`${config.prefix}`)) {
+      return message.channel.send(messagem);
+    }
+  }
+
+    var args = message.content.substring(config.prefix.length).split(" ");
+     if (!message.content.startsWith(config.prefix)) return;
+
+  let prefix = config.prefix;
+  let cmd = args.shift().toLowerCase();
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  let command =client.commands.get(cmd) || client.commands.get(client.aliases.get(cmd));
+  if(command.help.status === 'off')return message.channel.send('**Comando desabilitado!**');
+  if (command) {
+    command.run(client, message, args);
+  } else {
+    message.channel.send(
+      `:smile: **»** Tá bem man?! **${message.author.username}**, este comando é inexistente...`
+    );
+  }
+});
+
+client.on('raw', async dados => {
+  if(dados.t !== "MESSAGE_REACTION_ADD" && dados.t !== "MESSAGE_REACTION_REMOVE") return
+  if(dados.d.message_id != "715699132836085813") return
+
+  let servidor = client.guilds.cache.get("708025424545972297")
+  let membro = servidor.members.cache.get(dados.d.user_id)
+
+  let unverified = servidor.roles.cache.get('715686151993950229')
+  let cargo1 = servidor.roles.cache.get('715686285691453591')
+
+  if(dados.t === "MESSAGE_REACTION_ADD"){
+      if(dados.d.emoji.id === "708034328168366160"){
+          if(membro.roles.cache.has(cargo1)) return
+          membro.roles.add(cargo1)
+          membro.roles.remove(unverified);
+      }
+  }
+  if(dados.t === "MESSAGE_REACTION_REMOVE"){
+      if(dados.d.emoji.id === "708034328168366160"){
+          if(membro.roles.cache.has(cargo1)) return
+          membro.roles.remove(cargo1)
+          membro.roles.add(unverified);
+      }
+  }
 })
 
-
-nimouz.login(config.token);
+client.login(process.env.DISCORD_TOKEN);
